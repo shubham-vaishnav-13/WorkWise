03a4e0498fcb0ae63c0183bfbef24dbc81ab604b
shubham-vaishnav-13 <vaishnavshubham857@gmail.com>
2025-01-23 00:09:16 +0530

    Add templates for cleaning, repair, and kitchen sections; update URLs and views

--- WorkWiseHome/Homeapp/urls.py
+++ WorkWiseHome/Homeapp/urls.py
@@ -2,7 +2,12 @@ from django.urls import include, path
 
 from . import views
 urlpatterns = [
-    path('',views.home,name="home"),
+    path('',views.main,name="main"),
+    path('home/',views.home,name="home"),
+    path('cleaning/',views.cleaning,name="cleaning"),
+    path('repair/',views.repair,name="repair"),
+    path('kitchen/',views.kitchen,name="kitchen"),
+
 
 
 ]
--- WorkWiseHome/Homeapp/views.py
+++ WorkWiseHome/Homeapp/views.py
@@ -2,5 +2,14 @@ from django.shortcuts import render
 
 # Create your views here.
 
+def main(request):
+    return render(request,'base.html')
+
 def home(request):
-    return render(request,'Home.html')
+    return render(request,'home.html')
+def cleaning(request):
+    return render(request,'cleaning.html')
+def repair(request):
+    return render(request,'repair.html')
+def kitchen(request):
+    return render(request,'kitchen.html')
